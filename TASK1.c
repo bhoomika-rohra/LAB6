4606390afd740cdd76f2353bddcfe4918869d656
bhoomika-rohra <K250784@nu.edu.pk>
2025-10-02 13:11:19 +0500

    Create TASK1.c

---
+++ TASK1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+    int num,start,val;
+    printf("enter a number: \n");
+    scanf("%d",&num);
+    for(start=1;start<=num;num--){
+        printf("%d\n", num);
+    }
+}
